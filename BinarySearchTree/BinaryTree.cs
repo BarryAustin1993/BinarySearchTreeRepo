69a4a8208338b5cb19d5ca3b32c35990d296f4c2
unknown <ajpbarry@gmail.com>
2020-01-30 14:12:32 -0600

    added class node and binary tree, added three member variable to node with get sets:

---
+++ BinarySearchTree/BinaryTree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class BinaryTree
+    {
+
+        //Member Variables (HAS A)
+
+        //Constructor
+        BinaryTree()
+        {
+
+        }
+
+        //Member Methods (CAN DO)
+
+
+    }
+}
---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Node
+    {
+
+        //Member Variables (HAS A)
+        public Node LeftNode { get; set; }
+        public Node RightNode { get; set; }
+        public int Data { get; set; }
+        
+        //Constructor
+        Node()
+        {
+
+        }
+
+        //Member Methods (CAN DO)
+
+
+    }
+}
